e31a2fa259287c179dd086b6fee9a7ce3e28ff8c
Yogesh Raut <hiyogesh.raut@gmail.com>
2024-12-24 10:52:39 +0530

    created New Project component and Input reuseable compoenent

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
+import NewProject from "./components/NewProject";
 import ProjectsSider from "./components/ProjectSiderbar";
 
 function App() {
   return (
-    <main className="h-screen my-8">
+    <main className="h-screen my-8 flex gp-8">
       <ProjectsSider />
+      <NewProject />
     </main>
   );
 }
