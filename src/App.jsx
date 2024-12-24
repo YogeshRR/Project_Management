960f5de0724de40dc919721ee28282ecd306d44b
Yogesh Raut <hiyogesh.raut@gmail.com>
2024-12-24 10:19:13 +0530

    adds Project Sidebar with component

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
+import ProjectsSider from "./components/ProjectSiderbar";
+
 function App() {
   return (
-    <>
-      <h1 className="my-8 text-center text-5xl font-bold">Hello World</h1>
-    </>
+    <main>
+      <ProjectsSider />
+    </main>
   );
 }
 
---
+++ src/components/ProjectSiderbar.jsx
@@ -0,0 +1,10 @@
+export default function ProjectsSider () {
+    return <aside>
+        <button>
+            + Create a Project
+        </button>
+        <ul>
+            <li>Main Project</li>
+        </ul>
+    </aside>
+}
